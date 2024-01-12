1f5e20b2ee387dae40ea4821e1c71845b98b2b22
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-01-12 02:42:18 +0100

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div>
       Home
+      <button
+        onClick={() => {
+          navigate("/game");
+        }}
+        className="p-4 border-2 border-black mt-8"
+      >
+        Game
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
