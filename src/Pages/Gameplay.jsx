32a5fca47aff245683a45e3a1331a0d68fc79316
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-01-19 13:47:13 +0100

    commit

--- src/Pages/Gameplay.jsx
+++ src/Pages/Gameplay.jsx
@@ -1,17 +1,25 @@
-import React, { useEffect, useId } from "react";
-import { generateRandomColors } from "../gridGenerate";
+import React, { useEffect, useId, useState } from "react";
+import {
+  color_2x2_bg,
+  color_3x3_bg,
+  color_4x4_bg,
+  generateRandomColors,
+} from "../gridGenerate";
 import { useGridSettings } from "../../GridContext";
 import { useNavigate } from "react-router";
 
 const Gameplay = () => {
   const navigate = useNavigate();
+
+  const [gridStyle, setGridStyle] = useState("mainGrid-4x4");
+
   const {
     gridColorList,
-    setGridColorList,
+    // setGridColorList,
     totalColorNo,
-    setTotalColorNo,
+    // setTotalColorNo,
     gridColorNo,
-    setGridColorNo,
+    // setGridColorNo,
   } = useGridSettings();
 
   let randomColorsList = generateRandomColors(
@@ -38,11 +46,19 @@ const Gameplay = () => {
   //   );
   // }, []);
 
+  useEffect(() => {
+    if (gridColorList == color_2x2_bg) {
+      setGridStyle("mainGrid-2x2");
+    } else if (gridColorList == color_3x3_bg) {
+      setGridStyle("mainGrid-3x3");
+    } else setGridStyle("mainGrid-4x4");
+  }, [gridColorList]);
+
   return (
     <div>
       <div className={`w-12 h-12 mb-16 ${primaryColor}`}></div>
 
-      <div className="grid gap-8 mainGrid mx-auto">
+      <div className={`grid gap-8 ${gridStyle} mx-auto`}>
         {randomColors.map((color, index) => {
           return (
             <div
