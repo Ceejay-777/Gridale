6f159b8c6bf5d91673369d43f00bda5278434574
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-01-26 16:55:38 +0100

    commit

--- src/Pages/GameClassic.jsx
+++ src/Pages/GameClassic.jsx
@@ -9,17 +9,21 @@ import { useGridSettings } from "../../GridContext";
 import { useNavigate } from "react-router";
 
 const GameClassic = () => {
+  const clicksRef = useRef(0);
   const generate_2x2_grid = () => {
+    clicksRef.current = 0;
     setGridColorList(color_2x2_bg);
     return generateRandomColors(color_2x2_bg, 4, 4);
   };
 
   const generate_3x3_grid = () => {
+    clicksRef.current = 0;
     setGridColorList(color_3x3_bg);
     return generateRandomColors(color_3x3_bg, 9, 7);
   };
 
   const generate_4x4_grid = () => {
+    clicksRef.current = 0;
     setGridColorList(color_4x4_bg);
     return generateRandomColors(color_4x4_bg, 16, 13);
   };
@@ -27,22 +31,62 @@ const GameClassic = () => {
   const navigate = useNavigate();
   const [gridColorList, setGridColorList] = useState();
   const [gridStyle, setGridStyle] = useState("mainGrid-4x4");
-  const [randomColorsList] = useState(generate_4x4_grid);
+  const [randomColorsList, setRandomColorsList] = useState(generate_2x2_grid);
+  // const [randomColors, setRandomColors] = useState()
+  // const [primaryColor, setPrimaryColor] = useState()
   const totalTime = useRef(null);
   const countRef = useRef(0);
-  const clicksRef = useRef(0);
+  const mainGridRef = useRef();
+  const gridsCountRef = useRef(0);
 
   const [randomColors, primaryColor] = randomColorsList;
 
   const handleGridClick = (event) => {
     const thisClasslist = event.currentTarget.classList;
     if (thisClasslist.contains(primaryColor)) {
-      event.currentTarget.style.opacity = "0.5";
+      // event.currentTarget.style.opacity = "0.5";
       if (!thisClasslist.contains("clicked")) {
         thisClasslist.add("clicked");
         clicksRef.current += 1;
         console.log(clicksRef.current);
       }
+    }
+
+    if (
+      mainGridRef.current.classList.contains("mainGrid-2x2") &&
+      clicksRef.current === 1
+    ) {
+      nextGrid();
+    } else if (
+      mainGridRef.current.classList.contains("mainGrid-3x3") &&
+      clicksRef.current === 3
+    ) {
+      nextGrid();
+    } else if (
+      mainGridRef.current.classList.contains("mainGrid-4x4") &&
+      clicksRef.current === 4
+    ) {
+      nextGrid();
+    }
+  };
+
+  // useEffect(() => {
+  //   for (let i = 0; i < 4; i++) {
+  //     let random = generate_2x2_grid();
+  //     console.log(random);
+  //   }
+  // });
+
+  const nextGrid = () => {
+    gridsCountRef.current += 1;
+
+    if (gridsCountRef.current <= 5) {
+      setRandomColorsList(generate_2x2_grid);
+    } else if (gridsCountRef.current > 5 && gridsCountRef.current <= 8) {
+      console.log("Okay");
+      // setRandomColorsList(generate_3x3_grid);
+    } else if (gridsCountRef.current > 8) {
+      setRandomColorsList(generate_4x4_grid);
     }
   };
 
@@ -73,7 +117,7 @@ const GameClassic = () => {
     <div>
       <div className={`w-12 h-12 mb-16 ${primaryColor}`}></div>
 
-      <div className={`grid gap-8 ${gridStyle} mx-auto`}>
+      <div className={`grid gap-8 ${gridStyle} mx-auto`} ref={mainGridRef}>
         {randomColors.map((color, index) => {
           return (
             <div
@@ -98,8 +142,8 @@ const GameClassic = () => {
         Start
       </button>
 
-      <button className="p-4 border-2 border-black mt-8" onClick={handleStop}>
-        Stop
+      <button className="p-4 border-2 border-black mt-8" onClick={nextGrid}>
+        Next
       </button>
     </div>
   );
